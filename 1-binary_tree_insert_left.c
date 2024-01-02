808dc0adfd93ae45999c910987b34ffc35984b27
kiptuidenis <kiptuidenis@gmail.com>
2024-01-02 23:53:00 +0300

    Updates

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,30 +12,30 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *left_node;
+	binary_tree_t *left_node;
 
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
 
-    left_node = binary_tree_node(parent, value);
-    if (left_node == NULL)
-    {
-        return (NULL);
-    }
+	left_node = binary_tree_node(parent, value);
+	if (left_node == NULL)
+	{
+		return (NULL);
+	}
 
-    if (parent->left == NULL)
-    {
-        parent->left = left_node;
-        left_node->parent = parent;
-        return (left_node);
-    }
+	if (parent->left == NULL)
+	{
+		parent->left = left_node;
+		left_node->parent = parent;
+		return (left_node);
+	}
 
-    left_node->parent = parent;
-    left_node->left = parent->left;
-    parent->left->parent = left_node;
-    parent->left = left_node;
+	left_node->parent = parent;
+	left_node->left = parent->left;
+	parent->left->parent = left_node;
+	parent->left = left_node;
 
-    return (left_node);
+	return (left_node);
 }
