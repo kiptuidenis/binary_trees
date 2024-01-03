799b1c0b90e686c8790785477b08d2c7f5b0dd60
kiptuidenis <kiptuidenis@gmail.com>
2024-01-03 09:31:47 +0300

    Updates

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,18 +6,19 @@
  * binary_tree_is_leaf - Deletes an entire binary tree.
  *
  * @node: A pointer to the node of the tree to check.
+ *
+ * Return: return 1 if node is a leaf, otherwise 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-    {
-        return (0);
-    }
-
-    if (node->left == NULL && node->right == NULL)
-    {
-        return (1);
-    }
-    return (0);
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
