2703a49aba87c773ac6db554657ab9ab795ec383
kiptuidenis <kiptuidenis@gmail.com>
2024-01-03 14:31:09 +0300

    Updates

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,7 @@
 #ifndef BINARY_H
 #define BINARY_H
+#include <stddef.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -31,6 +33,7 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif /* BINARY_H */
 
