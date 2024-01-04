670e6ec526c25660dc80c0cb7ff5b83c41ab6aef
kiptuidenis <kiptuidenis@gmail.com>
2024-01-04 15:28:50 +0300

    Updates

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree.
+ * binary_tree_is_full -Checks if binary tree is full.
  *
  * @tree: A pointer to the root node of the tree to measure the balance factor.
  *
@@ -13,20 +13,23 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-     if (tree == NULL) {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    /* If the node has no children, it's a leaf node and the tree is full */
-    if (tree->left == NULL && tree->right == NULL) {
-        return (1);
-    }
+/* If the node has no children, it's a leaf node and the tree is full */
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
 
-    /* If the node has both children, recursively check left and right subtrees */
-    if (tree->left && tree->right) {
-        return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
-    }
-
-    /* If any node has only one child, the tree is not full */
-    return (0);
+/* If the node has both children, recursively check left and right subtrees */
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	}
+/* If any node has only one child, the tree is not full */
+	return (0);
 }
