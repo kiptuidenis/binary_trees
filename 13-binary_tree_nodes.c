d0552be538c4d68764632dd352d5b0e2f4d84f9f
kiptuidenis <kiptuidenis@gmail.com>
2024-01-04 11:48:04 +0300

    Updates

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,21 +11,19 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-	 if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count = 1;
+	}
 
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
-    if (tree->left != NULL || tree->right != NULL) 
-    {
-        count = 1;
-    }
-
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
-
-    return (count);
+	return (count);
 }
